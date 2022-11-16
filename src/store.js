2a4569d03837de0c7610ad9047620880d6021750
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-16 18:24:53 +0100

    added series list

--- src/store.js
+++ src/store.js
@@ -2,8 +2,9 @@ import { reactive } from "vue"
 import axios from 'axios'
 
 export const store = reactive({
-    API_URL: "https://api.themoviedb.org/3/search/movie",
+    API_URL: "https://api.themoviedb.org/3/search/",
     results: [],
+    series: [],
     error: null,
     params: {
         api_key: "047ca6274f89e4b26bb7a49cbb2609b4",
@@ -11,11 +12,24 @@ export const store = reactive({
     },
     callApi(url) {
 
-        const newUrl = `${url}?api_key=${this.params.api_key}&query=${this.params.query}`
+        const newUrl = `${url}movie?api_key=${this.params.api_key}&query=${this.params.query}`
+
+        const seriesUrl = `${url}tv?api_key=${this.params.api_key}&query=${this.params.query}`
 
         axios.get(newUrl)
             .then(response => {
                 this.results = response.data.results
+                // this.series = response.data.results
+            })
+            .catch(err => {
+                console.error(err.message);
+                this.error = err.message
+            })
+
+        axios.get(seriesUrl)
+            .then(response => {
+
+                this.series = response.data.results
             })
             .catch(err => {
                 console.error(err.message);
